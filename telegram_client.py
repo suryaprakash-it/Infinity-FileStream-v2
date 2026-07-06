694c794c52277defe3a3840abd6283e252c59207
Suryaprakash <154657173+suryaprakash-it@users.noreply.github.com>
2026-07-07 02:03:02 +0530

    Create telegram_client.py

---
+++ telegram_client.py
@@ -0,0 +1,9 @@
+from pyrogram import Client
+from config import Config
+
+bot = Client(
+    "InfinityFileStream",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN
+)
