5dd7fa0d42ec66afddf4c2d127b21ee98bd1a544
Suryaprakash <154657173+suryaprakash-it@users.noreply.github.com>
2026-07-07 13:20:38 +0530

    Update telegram_client.py

--- telegram_client.py
+++ telegram_client.py
@@ -2,11 +2,11 @@ from pyrogram import Client
 from config import Config
 import os
 
-# The bot will now use the string session instead of creating a file
+# Use the environment variable to keep your session string secure
 bot = Client(
     "bot",
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    session_string=os.getenv("BQAeORcAmgVLl_iBa378uNagwtHxmIpVdoJDddbEVDcqm7WJS2JD2ClYkASPbwX2b5GJ_9lxYkgTchCyQWVkNBvrnt9n6vpJ_semPiiUk59Qa3l1Z29S4SscCw9qsi38Lnee3JjuuosMkt0CJRiTTHvXn8TOV5v-nFICpyBvBf0lpd-JKq2ot2kcerOhDk5lQ-vbdryEncwiWIUhCoIGqXp96Nny2My1649bPSCmhc-PsiLwzg3nEAp0d_MdSath_nPy6ADAA8twkXEFXUn97ropX-Bo1rq2uv15sZD4nsk1VnCRGYFIhpBeJV9EA1zBh-h6SbdYOJ_oAHcy7uEWzmoSSZGwzwAAAABrlaseAA")
+    session_string=os.getenv("SESSION_STRING") 
 )
